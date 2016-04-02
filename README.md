afe03e7552152fffd43b89b5f0609cd5340606bc
chetan <chetan.narian@gmail.com>
2016-04-02 10:38:22 +0530

    Added more options [margins & paddings]

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
 # cssutilities-options
 
 > CSS layout utilities with options
+
+### How to use ?
+
+```
+git clone https://github.com/chetanraj/cssutilities-options.git
+```
+
+```bash
+cd cssutilities-options
+```
+
+```js
+node cssutilities.js 
+```
+
+### Options
+---
+
+###### Which sizing element do you need
++ px
++ em
++ rem
++ %
+
+###### Range Start
+
+###### Range End
+
+###### Add margin
+
+###### Add padding
+
+###### Do you need More Utilities like Floats, Display and Overflow
--- cssutilities.js
+++ cssutilities.js
@@ -1,58 +1,78 @@
 var inquirer = require("inquirer");
 
-var options = [
-	{
-    type: "list",
-    name: "options",
-    message: "Which sizing element do you need ?",
-    choices: [ "px", "em", "rem", "%" ]
-  },
-	{
-    type: "input",
-    name: "rangestart",
-    message: "Range Start",
-    validate: function( value ) {
-      var valid = !isNaN(parseFloat(value));
-      return valid || "Please enter a number";
-    },
-    filter: Number
-  },
-	{
-    type: "input",
-    name: "rangeend",
-    message: "Range End",
-    validate: function( value ) {
-      var valid = !isNaN(parseFloat(value));
-      return valid || "Please enter a number";
-    },
-    filter: Number
-  },
-	{
-    type: "confirm",
-    name: "more",
-    message: "Do you need More Utilities like Floats, Display and Overflow",
-    default: false
-  }
-];
+var options = [{
+	type: "list",
+	name: "options",
+	message: "Which sizing element do you need ?",
+	choices: ["px", "em", "rem", "%"]
+}, {
+	type: "input",
+	name: "rangestart",
+	message: "Range Start",
+	validate: function(value) {
+		var valid = !isNaN(parseFloat(value));
+		return valid || "Please enter a number";
+	},
+	filter: Number
+}, {
+	type: "input",
+	name: "rangeend",
+	message: "Range End",
+	validate: function(value) {
+		var valid = !isNaN(parseFloat(value));
+		return valid || "Please enter a number";
+	},
+	filter: Number
+}, {
+	type: "confirm",
+	name: "margin",
+	message: "Add margin ?",
+	default: true
+}, {
+	type: "confirm",
+	name: "padding",
+	message: "Add padding",
+	default: true
+}, {
+	type: "confirm",
+	name: "more",
+	message: "Do you need More Utilities like Floats, Display and Overflow",
+	default: false
+}];
 
-inquirer.prompt(options, function( answers ) {
-	 var css = '';
-	 
-	 for(var i = answers.rangestart; i <= answers.rangeend; i++) {
-		 css += ".m"+i+"{margin:"+i+""+answers.options+";}";
-		 
-		 css += ".mt"+i+"{margin-top:"+i+""+answers.options+";}";
-		 
-		 css += ".mr"+i+"{margin-right:"+i+""+answers.options+";}";
-		 
-		 css += ".mb"+i+"{margin-bottom:"+i+""+answers.options+";}";
-		 
-		 css += ".ml"+i+"{margin-left:"+i+""+answers.options+";}";
-	 }
-	 
-	 if(answers.more) {
-		 css += ".db{display:block}.dib{display:inline-block}.di{display:inline}.dt{display:table}.dtc{display:table-cell}.fl{float:left}.fr{float:right}.oh{overflow:hidden}.cb,.clear{clear:both}";
-	 }
-	 
-	 console.log(css);
+inquirer.prompt(options, function(answers) {
+	var css = '';
+
+	for (var i = answers.rangestart; i <= answers.rangeend; i++) {
+
+		if (answers.margin) {
+			css += ".m" + i + "{margin:" + i + "" + answers.options + ";}";
+
+			css += ".mt" + i + "{margin-top:" + i + "" + answers.options + ";}";
+
+			css += ".mr" + i + "{margin-right:" + i + "" + answers.options + ";}";
+
+			css += ".mb" + i + "{margin-bottom:" + i + "" + answers.options + ";}";
+
+			css += ".ml" + i + "{margin-left:" + i + "" + answers.options + ";}";
+		}
+
+		if (answers.padding) {
+			css += ".p" + i + "{padding:" + i + "" + answers.options + ";}";
+
+			css += ".pt" + i + "{padding-top:" + i + "" + answers.options + ";}";
+
+			css += ".pr" + i + "{padding-right:" + i + "" + answers.options + ";}";
+
+			css += ".pb" + i + "{padding-bottom:" + i + "" + answers.options + ";}";
+
+			css += ".pl" + i + "{padding-left:" + i + "" + answers.options + ";}";
+		}
+	}
+
+	if (answers.more) {
+		css += ".db{display:block}.dib{display:inline-block}.di{display:inline}.dt{display:table}.dtc{display:table-cell}.fl{float:left}.fr{float:right}.oh{overflow:hidden}.cb,.clear{clear:both}";
+	}
+
+	console.log(css);
 });
